65262eedf56744f7580cbc9e164c144d4d8fc5c9
Julien Vernet <julien@vernet.me>
2015-09-09 17:38:45 +0700

    Fix assets path

--- includes/scripts.php
+++ includes/scripts.php
@@ -20,8 +20,8 @@ add_action( 'admin_enqueue_scripts', 'bf_admin_scripts' );
  * @return void
  */
 function bf_admin_scripts() {
-	wp_register_script( 'bf-select2', BH_PATH . 'assets/js/vendor/select2.min.js', array( 'jquery' ), '3.5.2', true );
-	wp_register_script( 'bf-main', BH_PATH . 'assets/js/behavior-flow.js', array( 'jquery' ), BF_VERSION, true );
+	wp_register_script( 'bf-select2', BH_URL . 'assets/js/vendor/select2.min.js', array( 'jquery' ), '3.5.2', true );
+	wp_register_script( 'bf-main', BH_URL . 'assets/js/behavior-flow.js', array( 'jquery' ), BF_VERSION, true );
 	wp_enqueue_script( 'bf-select2' );
 	wp_enqueue_script( 'bf-main' );
 }
@@ -34,6 +34,6 @@ add_action( 'admin_enqueue_scripts', 'bf_admin_stylesheets' );
  * @return void
  */
 function bf_admin_stylesheets() {
-	wp_register_style( 'bf-main', BH_PATH . 'assets/css/behavior-flow.css', false, BF_VERSION, 'all' );
+	wp_register_style( 'bf-main', BH_URL . 'assets/css/behavior-flow.css', false, BF_VERSION, 'all' );
 	wp_enqueue_style( 'bf-main' );
 }
